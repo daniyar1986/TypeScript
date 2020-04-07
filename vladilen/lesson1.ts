3ce2f9b48cd6fb58513abc18a1cb8a7bad1b6a42
DMyrzakanov <Danntu1986#>
2020-04-07 11:17:11 +0600

    Первый урок есть продолжение

---
+++ vladilen/lesson1.ts
@@ -0,0 +1,15 @@
+//string
+let str : string = 'String';
+console.log(str);
+
+// number
+let num : number =10;
+console.log(num);
+
+//boolean
+
+let isTrue : boolean = true;
+let isFalse: boolean = false;
+console.log(isTrue);
+console.log(isFalse)
+
